6284f812908fc1ca7d319c97dde068968846811f
Sebastien Moreau <sebastien.moreau@osedea.com>
2021-01-13 22:15:45 +0100

    Removed registerWith function

--- android/src/main/java/com/flightcom/barometer/BarometerPlugin.java
+++ android/src/main/java/com/flightcom/barometer/BarometerPlugin.java
@@ -6,14 +6,9 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-// import io.flutter.plugin.common.MethodCall;
-// import io.flutter.plugin.common.MethodChannel;
-// import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-// import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** BarometerPlugin */
 // public class BarometerPlugin implements FlutterPlugin, MethodCallHandler {
@@ -36,23 +31,6 @@ public class BarometerPlugin implements FlutterPlugin {
 
     final Context context = flutterPluginBinding.getApplicationContext();
     setupEventChannels(context, flutterPluginBinding.getBinaryMessenger());    
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    // final MethodChannel channel = new MethodChannel(registrar.messenger(), "barometer");
-    // channel.setMethodCallHandler(new BarometerPlugin());
-
-    BarometerPlugin plugin = new BarometerPlugin();
-    plugin.setupEventChannels(registrar.context(), registrar.messenger());
   }
 
   // @Override
